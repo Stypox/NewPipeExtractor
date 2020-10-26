e7ad9cb25f375da15e451d0f2bd427832be4daa5
Stypox <stypox@pm.me>
2020-10-26 21:13:00 +0100

    Add har from TeamNewPipe/NewPipeExtractor#439

--- extractor/src/test/java/org/schabi/newpipe/extractor/services/youtube/mock/YoutubeStreamExtractorMockedTest.java
+++ extractor/src/test/java/org/schabi/newpipe/extractor/services/youtube/mock/YoutubeStreamExtractorMockedTest.java
@@ -12,12 +12,16 @@ import org.schabi.newpipe.extractor.exceptions.ParsingException;
 import org.schabi.newpipe.extractor.search.SearchExtractor;
 import org.schabi.newpipe.extractor.services.youtube.extractors.YoutubeStreamExtractor;
 import org.schabi.newpipe.extractor.stream.StreamExtractor;
+import org.schabi.newpipe.extractor.stream.VideoStream;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.List;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.schabi.newpipe.extractor.ExtractorAsserts.assertIsSecureUrl;
 import static org.schabi.newpipe.extractor.ServiceList.YouTube;
 
 public class YoutubeStreamExtractorMockedTest {
@@ -45,5 +49,29 @@ public class YoutubeStreamExtractorMockedTest {
         StreamExtractor extractor = getExtractor(
                 "https://www.youtube.com/embed/tPADtMN8lkI", "youtube_stream_4278.har");
         assertNotNull(extractor.getName());
+    }
+
+    @Test
+    public void test439() throws Exception {
+        StreamExtractor extractor = getExtractor(
+                "https://www.youtube.com/watch?v=tcYodQoapMg&pbj=1", "youtube_stream_439.har");
+        assertNotNull(extractor.getName());
+
+        final List<VideoStream> videoStreams = extractor.getVideoStreams();
+        final List<VideoStream> videoOnlyStreams = extractor.getVideoOnlyStreams();
+        assertNotNull(videoStreams);
+        assertNotNull(videoOnlyStreams);
+        videoStreams.addAll(videoOnlyStreams);
+
+        assertFalse(videoStreams.isEmpty());
+        for (final VideoStream stream : videoStreams) {
+            assertIsSecureUrl(stream.getUrl());
+            assertFalse(stream.getResolution().isEmpty());
+
+            final int formatId = stream.getFormatId();
+            // see MediaFormat: video stream formats range from 0 to 0x100
+            assertTrue("format id does not fit a video stream: " + formatId,
+                    0 <= formatId && formatId < 0x100);
+        }
     }
 }
